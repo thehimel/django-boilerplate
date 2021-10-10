ab3f685fc84157bdf349878c5c995f88513b53e3
Himel Das <thehimelofficial@gmail.com>
2021-10-10 17:34:28 +0200

    updated static files settings

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -110,9 +110,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static_files']
-STATIC_ROOT = BASE_DIR / 'static'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Media files (Uploaded by the users)
 MEDIA_URL = '/media/'
